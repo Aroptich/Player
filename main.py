cbf5f540ae04ab2a67051dd709fdfb7f18228f91
Aroptich <113030792+Aroptich@users.noreply.github.com>
2023-01-13 08:20:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
 import os
-import time
-from playsound import playsound
 from media import Ui_MainWindow
 from PyQt5.QtCore import QUrl, QUrlQuery, QMimeData
 from PyQt5.QtGui import *
@@ -49,20 +47,12 @@ class App(QMainWindow):
                                                 "All Files (*.*);; Mp3 Files (*.mp3)")
 
         list_of_paths = filename[0]
-        # self.playlist = [ i for i in list_of_paths if i not in self.playlist]
         self.playlist += list_of_paths
         list_of_track_name = [os.path.split(i)[1] for i in self.playlist]
-        # dict_of_track_names_paths = dict(zip(list_of_track_name, list_of_paths))
         self.ui.listWidget.addItems(list_of_track_name)
 
 
-    def playlist(self):
-        pass
 
-    #
-    # def load_file_list_widget(self, track_name):
-    #     print(f'load_file_list_widget: {track_name}')
-    #     self.ui.listWidget.addItems(track_name)
 
 
     #
@@ -76,22 +66,13 @@ class App(QMainWindow):
         self.ui.label_volume_2.setText(str(value)) # Задаем значение громкости текущего положения ползунка громкости
 
     def play(self):
-        # if self.state == "Play":
-        #     self.ui.play_btn.setText("Pause")
-        #     self.state = "Pause"
         position = self.info()
         print(f'Текущая позиция трека: {position}')
         url = QUrl.fromLocalFile(self.playlist[position])
         content = QMediaContent(url)
         self.player.setMedia(content)
-
         self.player.play()
-        # else:
-        #     self.ui.play_btn.setText("Play")
-        #     self.state = "Play"
-        #     self.player.pause()
-        #     paused = self.player.position()
-        #     self.position = paused
+      
 
         self.duration()
     def duration(self):
@@ -106,10 +87,6 @@ class App(QMainWindow):
         self.ui.duration_slider.setMaximum(duration)
         value = self.ui.duration_slider.value()
         self.ui.label_2.setText(str(value))
-        # print(a := self.ui.duration_slider.blockSignals(True))
-
-    # def pause(self):
-    #     self.player.pause()
 
 
     def stop(self):
@@ -121,23 +98,9 @@ class App(QMainWindow):
     def pause(self):
         self.player.pause() # Ставит трек на паузу
 
-    #
-
-
-
-
-
-
-
-
-
+ 
     def play_track(self):
-        # full_file_path = os.path.join(os.getcwd(), 'harrison_-_touch_me_muzati.net.mp3') # Получаем полный путь к треку
-        # url = QUrl.fromLocalFile(full_file_path) # Конвертируем полученный путь в URL
-        # content = QMediaContent(url) # передаем URL в медиаконтент
-        # self.ui.player.setMedia(content) # Передаем полученный URL в класс QMediaplayer
-        # self.ui.player.play() # Запускаем на проигрывание трек
-        if self.state == "Play":
+       if self.state == "Play":
             self.ui.play_btn.setText("Pause")
             self.state = "Pause"
             path = self.ui.listWidget.currentItem().text()
